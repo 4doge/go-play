2f0d38a94d4122b99b774f2646863b107716cbfe
4doge <antonboksha@gmail.com>
2019-09-18 17:04:02 +0300

    add type conversion

---
+++ 006-type-conversion/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var a int
+type mycustomtype int
+var b mycustomtype
+
+func main() {
+	a = 12
+	b = 13
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+	c := int(b)
+	fmt.Println(c)
+	fmt.Printf("%T\n", c)
+}
