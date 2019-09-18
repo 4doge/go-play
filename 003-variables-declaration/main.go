02df2b4541097004c669a5c94b88bb588bc64004
4doge <antonboksha@gmail.com>
2019-09-18 16:19:19 +0300

    add info about variables declaration

---
+++ 003-variables-declaration/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var x = 10
+
+// define zero based variable
+// 0 for int
+// false for bool
+// 0.0 for float
+// "" for string
+// nil for pointers / functions / interfaces / slices / channels / maps
+var y int
+
+func main() {
+	z := 15
+	fmt.Println("x - ", x)
+	fmt.Println("y - ", y)
+	fmt.Println("z - ", z)
+}
