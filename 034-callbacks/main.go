d9443a53a6dbdd37b2d593abc48309e6f28ceec3
4doge <antonboksha@gmail.com>
2019-09-21 23:36:15 +0300

    add callbacks

---
+++ 034-callbacks/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{11, 22, 33, 44, 55, 66, 77, 88, 99}
+	s := sum(nums...)
+	fmt.Println(s)
+	s2 := sumEven(sum, nums...)
+	fmt.Println(s2)
+}
+
+func sum(nums ...int) int {
+	total := 0
+	for _, v := range nums {
+		total += v
+	}
+	return total
+}
+
+func sumEven(f func(nums ...int) int, nums...int) int {
+	evenNums := []int{}
+	for _, v := range nums {
+		if v % 2 == 0 {
+			evenNums = append(evenNums, v)
+		}
+	}
+	return f(evenNums...)
+}
