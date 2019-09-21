dfe9f26167fb87f234d0e3021c2089d1c726a905
4doge <antonboksha@gmail.com>
2019-09-22 00:06:41 +0300

    update recursion, factorial via loop version

--- 036-recursion/main.go
+++ 036-recursion/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	f := factorial(5)
 	fmt.Println(f)
+	f2 := factorial2(5)
+	fmt.Println(f2)
 }
 
 func factorial(n int) int {
@@ -12,4 +14,12 @@ func factorial(n int) int {
 		return 1
 	}
 	return n * factorial(n-1)
+}
+
+func factorial2(n int) int {
+	total := 1
+	for i := 1; i <= n; i++ {
+		total *= i
+	}
+	return total
 }
