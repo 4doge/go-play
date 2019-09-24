17765703c8f3baa463b3b6fcfca1668b5ecc75f9
4doge <antonboksha@gmail.com>
2019-09-24 20:11:23 +0300

    add directional channels

---
+++ 047-directional-channels/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan<- int, 2)
+	c <- 13
+	c <- 99
+	// can't read from `send-only` channel
+	// fmt.Println(<-c)
+
+	c2 := make(<-chan int, 2)
+	// can't send to `receive-only` channel
+	// c2 <- 13
+	// c2 <- 13
+	fmt.Println(<-c2)
+}
